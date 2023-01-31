a9652397338b7353712802c1b11c29797f73aa41
Ruchitha Chinta <122854121+ruchitha36@users.noreply.github.com>
2023-01-31 19:48:34 +0530

    Add files via upload

---
+++ sep1.py
@@ -0,0 +1,12 @@
+"""  sep method  """
+
+# print("Hello" , "World")
+# print("Hello"  "World")  
+# print("Hello"+'-'+"World") 
+# print("Hello"  "World",'Core', 'Python',sep="")
+# print("Hello"  "World",'Core', 'Python',sep="-")
+# print("Hello" , "World",'Core', 'Python',sep="-",)
+# print("Hello" , "World",'Core', 'Python',sep="-ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" "ok")
+# print("Hello" , "World",'Core', 'Python',sep="-"+ "ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" ' ' "ok")
